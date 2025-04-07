453f0b8fabaa174eb0fda8ff74b45d00261ba0d5
ravi_s <x23411627@student.ncirl.ie>
2025-04-07 20:31:46 +0100

    Added assets to generate visualization images for enterprises

--- enterprises/assets/constants.py
+++ enterprises/assets/constants.py
@@ -1,3 +1,10 @@
 ENTERPRISES_RAW_FILE_PATH = "data/raw/enterprises_raw.csv"
 
 TOP_ECONOMIC_FILE_PATH = "data/outputs/top_enterprises.png"
+
+
+COUNTRIES_WITH_MOST_ENTERPRISES_FILE_PATH = "data/outputs/countries_with_most_enterprises.png"
+COUNTRIES_WITH_LEAST_ENTERPRISES_FILE_PATH = "data/outputs/countries_with_least_enterprises.png"
+CITIES_WITH_MOST_ENTERPRISES_FILE_PATH = "data/outputs/cities_with_most_enterprises.png"
+CITIES_WITH_LEAST_ENTERPRISES_FILE_PATH = "data/outputs/cities_with_least_enterprises.png"
+CITIES_HISTOGRAM_FILE_PATH = "data/outputs/cities_histogram.png"
--- enterprises/assets/enterprise.py
+++ enterprises/assets/enterprise.py
@@ -6,6 +6,7 @@ import pandas as pd
 import requests
 import seaborn as sns
 import matplotlib.pyplot as plt
+import plotly
 
 from dagster import asset, AssetExecutionContext
 
@@ -13,6 +14,7 @@ from . import constants
 from .constants import ENTERPRISES_RAW_FILE_PATH
 
 LOGGER_CONFIG = {"loggers": {"console": {"config": {"log_level": "INFO"}}}}
+
 
 def remove_alphabets(value: str) -> str:
     if isinstance(value, str):
@@ -45,18 +47,69 @@ def enterprises_file() -> None:
 @asset(deps=["enterprises_file"], required_resource_keys={"mongo"})
 def enterprises(context) -> None:
     df = pd.read_csv(ENTERPRISES_RAW_FILE_PATH)
-    enterprises = df.reset_index().to_dict("records")
-    context.resources.mongo["enterprises"].insert_many(enterprises)
+    enterprise = df.reset_index().to_dict("records")
+    context.resources.mongo["enterprises"].insert_many(enterprise)
 
 
 @asset(deps=["enterprises"], required_resource_keys={"mongo"})
-def top_economic_countries(context: AssetExecutionContext) -> None:
+def countries_with_most_enterprises(context: AssetExecutionContext) -> None:
     apdv_enterprises_collection = context.resources.mongo["enterprises"]
-    enterprises = apdv_enterprises_collection.find()
-    df = pd.DataFrame(enterprises)
+    enterprise = apdv_enterprises_collection.find()
+    df = pd.DataFrame(enterprise)
     fig, ax = plt.subplots(figsize=(10, 10))
-    countries_all_enterprises = df[(df["year"] == 1998)].groupby(by="country").agg({'enterprises': 'sum'})
-    sns.barplot(countries_all_enterprises.sort_values(by="enterprises", ascending=True).tail(10)[::-1], x="country",
-                y="enterprises").set_title("Number of enterprises in top european countries in 1998")
-    plt.savefig(constants.TOP_ECONOMIC_FILE_PATH, format="png", bbox_inches="tight")
+    countries_all_enterprises = df.groupby(by="country").agg({'enterprises': 'sum'})
+    sns.barplot(countries_all_enterprises.sort_values(by="enterprises", ascending=False).head(10), x="country",
+                y="enterprises").set_title("Countries with most")
+    plt.savefig(constants.COUNTRIES_WITH_MOST_ENTERPRISES_FILE_PATH, format="png", bbox_inches="tight")
     plt.close(fig)
+
+
+@asset(deps=["enterprises"], required_resource_keys={"mongo"})
+def countries_with_least_enterprises(context: AssetExecutionContext) -> None:
+    df = find_enterprises(context)
+    fig, ax = plt.subplots(figsize=(10, 10))
+    countries_all_enterprises = (df.groupby(by="country")
+                                 .agg({'enterprises': 'sum'})
+                                 .sort_values(by="enterprises", ascending=True).tail(10))
+    (sns.barplot(countries_all_enterprises, x="country",y="enterprises")
+     .set_title("Countries with least enterprises"))
+    plt.savefig(constants.COUNTRIES_WITH_LEAST_ENTERPRISES_FILE_PATH, format="png", bbox_inches="tight")
+    plt.close(fig)
+
+@asset(deps=["enterprises"], required_resource_keys={"mongo"})
+def cities_with_most_enterprises(context: AssetExecutionContext) -> None:
+    df = find_enterprises(context)
+    fig, ax = plt.subplots(figsize=(10, 10))
+    countries_all_enterprises = (df.groupby(by="city")
+                                 .agg({'enterprises': 'sum'})
+                                 .sort_values(by="enterprises", ascending=False).tail(10))
+    (sns.barplot(countries_all_enterprises, x="city",y="enterprises")
+     .set_title("Cities with most enterprises"))
+    plt.savefig(constants.CITIES_WITH_MOST_ENTERPRISES_FILE_PATH, format="png", bbox_inches="tight")
+    plt.close(fig)
+
+@asset(deps=["enterprises"], required_resource_keys={"mongo"})
+def cities_with_least_enterprises(context: AssetExecutionContext) -> None:
+    df = find_enterprises(context)
+    fig, ax = plt.subplots(figsize=(10, 10))
+    countries_all_enterprises = (df.groupby(by="city")
+                                 .agg({'enterprises': 'sum'})
+                                 .sort_values(by="enterprises", ascending=True).tail(10))
+    (sns.barplot(countries_all_enterprises, x="city",y="enterprises")
+     .set_title("Cities with least enterprises"))
+    plt.savefig(constants.CITIES_WITH_LEAST_ENTERPRISES_FILE_PATH, format="png", bbox_inches="tight")
+    plt.close(fig)
+
+@asset(deps=["enterprises"], required_resource_keys={"mongo"})
+def cities_distribution(context: AssetExecutionContext) -> None:
+    df = find_enterprises(context)
+    fig, ax = plt.subplots(figsize=(10, 10))
+    df["city"].hist(bins=5, figsize=(20, 15))
+    plt.savefig(constants.CITIES_HISTOGRAM_FILE_PATH, format="png", bbox_inches="tight")
+    plt.close(fig)
+
+def find_enterprises(context):
+    apdv_enterprises_collection = context.resources.mongo["enterprises"]
+    enterprise = apdv_enterprises_collection.find()
+    df = pd.DataFrame(enterprise)
+    return df
